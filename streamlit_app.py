5963d866904d03857090e0050014abeb63c62bf9
houred <Houred2030@gmail.com>
2024-07-21 11:16:28 +0000

    101010

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
-st.title("Salam aalaykoum 99999")
+st.title("Salam aalaykoum 10101010")
 st.write("salam")
-st.text_input("ddddd")
-st.title("999999")
-st.title("999999")
+st.text_input("")
+st.title("10101010")
+st.title("10101010")
+st.button("test", disabled=True , key='5')
+if st.button("activation"):
+    st.button(disabled=False , key='5')
+
 
